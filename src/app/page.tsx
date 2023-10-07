b9cb217646c90519b623b752b99d85d27023d1c7
Abbas Mehdi <abbasmehdi692@gmail.com>
2023-10-07 14:02:05 +0500

    Initial Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+    <div className={styles.content}>
+       <h1>Abbas</h1>
+
+    </div>
+    </main>
+  )
+}
