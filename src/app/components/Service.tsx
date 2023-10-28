7c3a3ebe3558014ec92cd5ee8b4f1af736963c15
Abbas Mehdi <abbasmehdi692@gmail.com>
2023-10-29 02:52:02 +0500

    Eblingo Complete HomePage

--- src/app/components/Service.tsx
+++ src/app/components/Service.tsx
@@ -1,30 +1,38 @@
-import react from 'react';
+"use client";
+import react, { useState } from 'react';
 import { Button, Row, Col } from 'antd';
 import Image from 'next/image';
 import styles from '../styles/Service.module.css';
-import { start } from 'repl';
+import { useEffect } from 'react';
 
 const Service = () => {
+    const [scrollPosition, getScrollPositon] = useState(document.documentElement.scrollTop)
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            getScrollPositon(document.documentElement.scrollTop);
+        })
+    }, [])
     return (
         <div className={styles.main}>
             <div className={styles.Service}>
-            <Row>
-            <Col span={18}>
-            <div className={styles.Service_Heading}>
-            <h6 className={styles.allservice_heading}>OUR SERVICES</h6>
-            <div className={styles.heading_translation_firm}>
-                Biggest language Translation firm
+                <Row>
+                    <Col span={18}>
+                        <div className={styles.Service_Heading}>
+                            <h6 className={styles.allservice_heading}>OUR SERVICES</h6>
+                            <div className={styles.heading_translation_firm}>
+                                Biggest language Translation firm
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <Button className={styles.quote_button}>Get a quote</Button>
+                    </Col>
+                </Row>
             </div>
-            </div>
-            </Col>
-            <Col>
-            <Button className={styles.quote_button}>Get a quote</Button>
-            </Col>
-            </Row>
-            </div>
-            <div className={styles.First_Service_Section}>
-                <Row justify={'start'}>
-                    <Col span={10}>
+            <Row justify={'start'}>
+                <Col span={12}>
+                    <div className={styles.First_Service_Section}>
                         <h2 className={styles.service_heading}>
                             Audio Visual Solution
                         </h2>
@@ -35,21 +43,8 @@ const Service = () => {
                                 <li><div className={styles.list}>Closed Caption</div></li>
                             </ul>
                         </div>
-                    </Col>
-                    <Col>
-                        <Image
-                            src="/assets/services1.png"
-                            alt="Service"
-                            width={673}
-                            height={507}
-                            className={styles.service_box1}
-                        />
-                    </Col>
-                </Row>
-            </div>
-            <div className={styles.Second_Service_Section}>
-                <Row justify={'start'}>
-                    <Col span={10}>
+                    </div>
+                    <div className={styles.Second_Service_Section}>
                         <h2 className={styles.service_heading}>
                             Document Solutions
                         </h2>
@@ -60,21 +55,8 @@ const Service = () => {
                                 <li><div className={styles.list}>Technical Translation</div></li>
                             </ul>
                         </div>
-                    </Col>
-                    <Col>
-                        <Image
-                            src="/assets/services2.png"
-                            alt="Service"
-                            width={673}
-                            height={507}
-                            className={styles.service_box1}
-                        />
-                    </Col>
-                </Row>
-            </div>
-            <div className={styles.third_Service_Section}>
-                <Row justify={'start'}>
-                    <Col span={10}>
+                    </div>
+                    <div className={styles.third_Service_Section}>
                         <h2 className={styles.service_heading}>
                             Media Translation
                         </h2>
@@ -85,21 +67,9 @@ const Service = () => {
                                 <li><div className={styles.list}>Reduce Your workload</div></li>
                             </ul>
                         </div>
-                    </Col>
-                    <Col>
-                        <Image
-                            src="/assets/services3.png"
-                            alt="Service"
-                            width={673}
-                            height={507}
-                            className={styles.service_box1}
-                        />
-                    </Col>
-                </Row>
-            </div>
-            <div className={styles.fourth_Service_Section}>
-                <Row justify={'start'}>
-                    <Col span={10}>
+
+                    </div>
+                    <div className={styles.fourth_Service_Section}>
                         <h2 className={styles.service_heading}>
                             Publishing Partnership
                         </h2>
@@ -110,26 +80,48 @@ const Service = () => {
                                 <li><div className={styles.list}>Magzine  and media publication</div> </li>
                             </ul>
                         </div>
-                    </Col>
-                    <Col>
+
+                    </div>
+                </Col>
+                <Col span={12}>
+                    <div className={styles.Images}>
+
+                        <Image
+                            src="/assets/services1.png"
+                            alt="Service"
+                            width={673}
+                            height={507}
+                            className={`${styles.Service_Image} ${scrollPosition >= 300 && scrollPosition < 900 ? styles.active : ''
+                                }`}
+                        />
+                        <Image
+                            src="/assets/services2.png"
+                            alt="Service"
+                            width={673}
+                            height={507}
+                            className={`${styles.Service_Image} ${scrollPosition >= 900 && scrollPosition < 1500 ? styles.active : ''
+                                }`}
+                        />
+                        <Image
+                            src="/assets/services3.png"
+                            alt="Service"
+                            width={673}
+                            height={507}
+                            className={`${styles.Service_Image} ${scrollPosition >= 1500 && scrollPosition < 2100 ? styles.active : ''
+                                }`}
+                        />
                         <Image
                             src="/assets/services4.png"
                             alt="Service"
                             width={673}
                             height={507}
-                            className={styles.service_box1}
+                            className={`${styles.Service_Image} ${scrollPosition >= 2100 && scrollPosition < 2400 ? styles.active : ''
+                                }`}
                         />
-                    </Col>
-                </Row>
-            </div>
-            <div className={styles.brands_images}>
-                <Image
-                    src="/assets/brand.png"
-                    alt="Service"
-                    width={1442}
-                    height={146}
-                />
-            </div>
+
+                    </div>
+                </Col>
+            </Row>
 
         </div>
 
