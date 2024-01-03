8e75826a8c32c41eadd9e14b2363178c6d895188
Abbas Mehdi <abbasmehdi692@gmail.com>
2024-01-03 16:01:15 +0500

    update eblingo

--- src/app/axiosInterceptor/axiosInterceptor.tsx
+++ src/app/axiosInterceptor/axiosInterceptor.tsx
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:3001', 
+  baseURL: 'http://193.203.167.24:3001/', 
 });
 
 export default api;
